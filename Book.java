b021ac782eccac58df833eca7d67335142eb0592
ChrisReyes96 <47114140+ChrisReyes96@users.noreply.github.com>
2019-02-04 12:44:00 -0500

    work in class
    changes made in class

--- Book.java
+++ Book.java
@@ -11,16 +11,56 @@ class Book
      // The fields.
     private String author;
     private String title;
-
+    private int pages;
+    private String refNumber;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
     }
 
     // Add the methods here ...
+     public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void printAuthor()
+    {
+        System.out.println(author);
+    }
+    
+    public void printTitle()
+    {
+        System.out.println(title);
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    public void printDetails()
+    {
+        System.out.println("Title: "+ title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Reference number: " + refNumber);
+    }
+    
+    public void setRefNumber (String ref)
+    {
+        get
+    }
 }
